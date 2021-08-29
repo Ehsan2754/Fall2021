a7d8e269df22ae226ae36729df642ee2acd0e1e9
Ehsan2754 <ehsan2754@gmail.com>
2021-08-30 00:06:14 +0300

    UDP File transfer

--- DPN/Lab3/client.py
+++ DPN/Lab3/client.py
@@ -1,9 +1,12 @@
-from DPN.Lab3.server import TIMEOUT
-from _typeshed import Self
+# from functools import cmp_to_key
+# from DPN.Lab3.server import RES, TIMEOUT
+# from _typeshed import Self
+# from DPN.Lab3.server import InvalidInput
 import socket
 from enum import Enum
 # CONSTANTS
-PAYLOAD_SIZE = 1472 # maximum payload for ETHERNET frame using UDP header
+PAYLOAD_SIZE_t = 1472 # maximum payload for ETHERNET frame using UDP header
+RESERVE = 7 # D | XXXX<seq.num> | <DATA>
 TRIAL_NUM = 5
 # TODO
 # SUPPORT BINARY
@@ -12,32 +15,34 @@ TRIAL_NUM = 5
 QUIT_CMD = 'quit'
 KI_CMD = 'KI' # KeyboardInterrupt command
 EXIT_MSG = 'User has quit.'
-ENTRY_MSG = 'ENTER COMMAND :\n$'
+ENTRY_MSG = '''ENTER COMMAND:\nP\n'---->\tYOU WANT TO SEND FILE BY ENTERING THE PATH\nD\n'---->\tYOU WANT TO SEND A STRING TO THE SERVER WHICH WILL BE SAVED IN BINARY(.bin) FORMAT\n$'''
 
 IP_ADDR = 'localhost'
 PORT = 40047
 BUFFER_SIZE = 1518
 TIMEOUT = 0.5
+seq_no = 0
+
 def send_command(bin_str,socket,buffer_size,timeout):
     try:
-        BYTE2SEND = str.encode(bin_str)
+        BYTE2SEND = (bin_str)
         socket.settimeout(timeout)
         socket.sendto(BYTE2SEND, (IP_ADDR,PORT))
         RES=socket.recvfrom(buffer_size)[0].decode('utf-8')
         return RES
     except Exception as ex:
         raise ex
-
 class InvalidInputType(Exception):
     def __init__(self):
         self.args = ('Invalid type argument. use the enum class INPUT_TYPE to pass the parameter')    
 # Enum class defining the datagram type
-class INPUT_TYPE(Enum):
-    PATH='PATH'
-    DATA='DATA'
-class DATAGRAM_TYPE(Enum):
-    DATA = 'D'
-    START = 'S'
+class INPUT_TYPE:
+    PATH='d'
+    DATA='p'
+class DATAGRAM_TYPE:
+    DATA = 'd'
+    START = 's'
+    ACK = 'a'
 class EhsanChunker:
     # Exception for case of empty path or data
 
@@ -54,13 +59,11 @@ class EhsanChunker:
     @parm IP_ADDRESS: IPv4 of Server in string format default = 'localhost'
     @parm PORT: SERVER port default = 5000
     '''
-    def __init__(self,arg=None,type = INPUT_TYPE.DATA ,IP_ADDRESS='localhost' ,PORT=5000):
-        self.IP_ADDR = IP_ADDRESS
-        self.PORT = PORT
+    def __init__(self,arg=None,type = INPUT_TYPE.DATA):
         self.datagrams  = []
         if type == INPUT_TYPE.DATA:
             try:
-                self.BYTES2SEND = bytes(arg)
+                self.BYTES2SEND = str.encode(arg)
                 self.EXTENTION = 'bin'
             except Exception as ex:
                 raise ex
@@ -73,38 +76,101 @@ class EhsanChunker:
                 raise ex
         else:
             raise InvalidInputType
-        self.PACKET_LEN = len(self.BYTES2SEND)
-        for i in range (0,self.PACKET_LEN//PAYLOAD_SIZE):
-            self.datagrams.append(self.BYTES2SEND[i*PAYLOAD_SIZE:(i+1)*PAYLOAD_SIZE])
-        if (self.PACKET_LEN%PAYLOAD_SIZE) > 0:   
-            self.datagrams.append(self.BYTES2SEND[self.PACKET_LEN//PAYLOAD_SIZE*self.PAYLOAD_SIZE:])
-        self.datagrams = [T for T in enumerate(self.datagrams)]
+        self.SIZE = len(self.BYTES2SEND)    
+
+    def genCMD(self,CMD_TYPE,SEQ_NO,arg1=None,arg2=None,arg3=None,data=None):
+        res =  f'{CMD_TYPE}|{SEQ_NO}'
+        if arg1 : res+=f'|{arg1}'
+        if arg2 : res +=f'|{arg2}'
+        if arg3 : res +=f'|{arg3}'
+        print(data)
+        res = (str.encode(res+'|')+data) if data else str.encode(res)
+        return res
     
-    def send_pkt(self,socket,timeout):
-        for _ in range(5):
-            try:
-                pass
+    # to-do
+    # interpret incomming data.
+    def interpretCMD(self,cmd):
+        return cmd.split('|')
+
+    def chuck(self,chunkSize = PAYLOAD_SIZE_t): #divides packet into buffer size managed data
+        self.PACKET_LEN = len(self.BYTES2SEND)
+        for i in range (0,self.PACKET_LEN//chunkSize):
+            self.datagrams.append(self.BYTES2SEND[i*chunkSize:(i+1)*chunkSize])
+        if (self.PACKET_LEN%chunkSize) > 0:   
+            self.datagrams.append(self.BYTES2SEND[self.PACKET_LEN//chunkSize*chunkSize:])
+        self.datagrams = [T for T in enumerate(self.datagrams,1)] # labling packets by numbers from 1.
+
+
+    def send_pkt(self,socket,timeout=TIMEOUT):
+        server_resp = []
+        # ---- starting communication
+        for i in range(5):
+            try: # try to start communication with server
                 # start pkt
-            except socket.timeout:
-                continue
-
+                server_resp = self.interpretCMD(
+                    send_command(
+                    self.genCMD(DATAGRAM_TYPE.START,seq_no,self.EXTENTION,self.SIZE),
+                    socket,
+                    BUFFER_SIZE,
+                    timeout
+                )
+                )
+                print(
+                    'Client\t>>\tServer: REQUEST :\t','TYPE=',DATAGRAM_TYPE.START,
+                    '.:.\t','SEQ No.=',seq_no,
+                    '.:.\t','EXT.=',self.EXTENTION,
+                    '.:.\t','FILE-SIZE=',self.SIZE
+                )
+                break # breaks the loop if no timeout or other exception happened
+            # except socket.timeout as ex:
+            except Exception as ex:
+                print(ex)
+                if i == 4: # raises the timeout exception if the timeout happened 5 times(i=4)
+                    raise ex
+                else:      # if its less than five times trying it retries sending the command
+                    continue
+        print(server_resp)
+        RESP_TYPE = server_resp[0]
+        RESP_SEQNO = server_resp[1]
+        RESP_BUFFERSIZE = server_resp[2] 
+        self.chuck(PAYLOAD_SIZE_t-RESERVE)
+        print(self.datagrams)
+        print(
+            'Client\t<<\tServer: RESPOND :\t','TYPE=',RESP_TYPE,
+            '.:.\t','SEQ No.=',RESP_SEQNO,
+            '.:.\t','BUFFER SIZE=',RESP_BUFFERSIZE
+        )
         while(len(self.datagrams)):
-            for _ in range(5):
-                # data packs
-                pass
-        
-        for _ in range(5):
-            try:
-                pass
-                # end pkt
-            except socket.timeout:
-                continue
-        
-
-
-            
-
-
+            # ---- starting communication
+            data=self.datagrams.pop(0)
+            for i in range(5):
+                try: # try to send data chunks to server
+                    server_resp = self.interpretCMD(
+                        send_command(
+                        self.genCMD(CMD_TYPE=INPUT_TYPE.DATA,SEQ_NO=RESP_SEQNO,data=data[1]),
+                        socket,
+                        BUFFER_SIZE,
+                        timeout
+                    ))
+                    print(
+                        'Client\t>>\tServer: REQUEST :\t','TYPE=',DATAGRAM_TYPE.DATA,
+                        '.:.\t','SEQ No.=',RESP_SEQNO,
+                        '.:.\tDATA=\n',data
+                    )
+                    RESP_TYPE = server_resp[0]
+                    RESP_SEQNO = server_resp[1]
+                    print(
+                         'Client\t<<\tServer: RESPOND :\t','TYPE=',RESP_TYPE,
+                         '.:.\t','SEQ No.=',RESP_SEQNO,
+                         '.:.\t','SIZE=',RESP_BUFFERSIZE
+                    )
+                    break # breaks the loop if no timeout or other exception happened
+                # except socket.timeout as ex:
+                except Exception as ex:
+                    if i == 4: # raises the timeout exception if the timeout happened 5 times(i=4)
+                        raise ex
+                    else:      # if its less than five times trying it retries sending the command
+                        continue
 
 
 if __name__ == '__main__':
@@ -114,13 +180,25 @@ if __name__ == '__main__':
         while True:      # infinite loop
             cmd = input(ENTRY_MSG).lower() # getting user input command and converting all to lowercase letters
             if cmd.lower() == QUIT_CMD:    # check if user entered QUIT command, break the loop
-                send_command(QUIT_CMD,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE) # send QUIT command to server
+                # send_command(QUIT_CMD,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE) # send QUIT command to server
                 break
-            print(send_command(cmd,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE)) # send the command to UDP-server and get the respose
+            dtype = ''
+            if cmd == 'd':
+                dtype = INPUT_TYPE.DATA 
+            elif cmd == 'p':
+                dtype = INPUT_TYPE.PATH 
+            else:
+                raise InvalidInputType
+            arg = input('Enter the <PATH> OR the STRING you want to send.')
+            # print(send_command(cmd,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE)) # send the command to UDP-server and get the respose
+            
+            chunks = EhsanChunker(arg = arg,
+            type=dtype)
+            chunks.send_pkt(UDPClientSocket,TIMEOUT)
         UDPClientSocket.close()
         print(EXIT_MSG)  # print EXIT message in case of manual quit command
     except KeyboardInterrupt:
-        send_command(KI_CMD,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE) # send KeyboardInterrupt command to server
+        # send_command(KI_CMD,UDPClientSocket,IP_ADDR,PORT,BUFFER_SIZE) # send KeyboardInterrupt command to server
         UDPClientSocket.close()
         print(EXIT_MSG) # print EXIT message in case of KeyboardInterrupt 
         
--- DPN/Lab3/server.py
+++ DPN/Lab3/server.py
@@ -11,88 +11,26 @@ REQ_MSG = '>> REQUEST RECEIVED FROM (IP={0},PORT={1}): ${2}'
 TIMEOUT_MSG = '! SERVER LISTENING TIMEOUT AFTER {0}s.'
 IP_ADDR = 'localhost'
 PORT = 40047
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 1518
 TIMEOUT = 59.9
-
+PATH = './'
 # EXCEPTOPNS
-class CALC_EX(Exception):
-    pass
-class EmptyInput(CALC_EX):
-    msg = '\t! EMPTY INPUT'
-class InvalidInput(CALC_EX):
-    msg = '\t! INVALID INPUT'
-class InvalidOperator(CALC_EX):
-    msg = '\t! INVALID OPERATOR'
-    def __init__(self,opt):
-        self.opt = '\t' + opt
-class InvalidArgs(CALC_EX):
-    msg = '\t! INVALID ARGUMENTS'
-    def __init__(self,arg):
-        self.arg = '\t' + arg
+import random
+
 class QuitCommandEx(Exception):
     msg = '\t! QUIT COMMAND RECEIVED'
 
-
-
-
-'''
-    evaluateOperatorAndArguments(arg):
-    evaluates the supported operators and argumetns in a  string list of length 3 
-'''
-def evaluateOperatorAndArguments(arg):
-    OPERATOR = arg[0]
-    if OPERATOR in OPERATIONS_CMD: #checks if the operator is available on the list
-        try:
-            left_operand = ''
-            right_operand = ''
-            if ('.' in arg[1]) or ('.' in arg[2]): # checks if arguments contain point decclearing the argument type (float or int)
-                left_operand = float(arg[1]) # tries to convert the inputs to float
-                right_operand = float(arg[2])
-            else:
-                left_operand = int(arg[1])  # tries to convert the inputs to interger
-                right_operand = int(arg[2])
-            return str(eval('left_operand'+OPERATOR+'right_operand')) # returns the operation result
-        except ValueError as ex:    # raises the ValueError in case of inappropriate argument
-            raise InvalidArgs(arg[1]+' OR/AND '+arg[2])
-        except ArithmeticError as ex:
-            raise ex
-            
-    else:
-        raise InvalidOperator(opt=OPERATOR) # raises invalid exception in case of failure
-
-    
-
-'''
-    evaluateInput(cmd):
-    evaluates the inputs in terms of length and adminstrative commands and returns the standard 3 member argument list
-'''
-def evaluateInput(cmd):  
-    ARGS = cmd.split() # Split the input string for evaluation
-    # print(ARGS)
-    if len(ARGS)<ARG_LEN: # Check the length of the arguments comparting to the ARG_LEN constant
-        if len(ARGS) == 0 or ARGS == None: #if the command is empty
-            raise  EmptyInput
-        if len(ARGS) == 1 : # check if the command is  a quit/KeyboardInterrupt
-            if ARGS[0] == KI_CMD :
-                raise KeyboardInterrupt      # raising KeyboardInterrupt excetpion
-            if ARGS[0].lower() == QUIT_CMD :
-                raise QuitCommandEx             # raising Quit exception
-            else: 
-                raise InvalidInput # in other cases the command is Invalid!
-        else: 
-            raise InvalidInput # in other cases the command is Invalid!
-    if len(ARGS)==ARG_LEN: # if the length of input command matches the ARG_LEN constant
-        return ARGS
-    if len(ARGS)>ARG_LEN : # if the length of input command is more than ARG_LEN constant raise the InvalidInput excception
-        raise InvalidInput()        
-    
-def respond (input):
-    RES = ' >> SERVER RESULT : ' +str(
-        evaluateOperatorAndArguments(
-            evaluateInput(
-                input)))
-    print(RES)
-    return RES
+class DATAGRAM_TYPE:
+    DATA = 'd'
+    START = 's'
+    ACK = 'a'
+def genCMD(CMD_TYPE,SEQ_NO,arg1=None,arg2=None,arg3=None,data=None):
+    res =  f'{CMD_TYPE}|{SEQ_NO}'
+    if arg1 : res+=f'|{arg1}'
+    if arg2 : res +=f'|{arg2}'
+    if arg3 : res +=f'|{arg3}'
+    res = str.encode(res+'|')+data if data else str.encode(res)
+    return res
 if __name__ == '__main__':
     try:
         UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) # Creating the UDP socket
@@ -101,12 +39,16 @@ if __name__ == '__main__':
         print(READY_MSG)
         while True:
             BYTE_ADDRESS_PAIRS = UDPServerSocket.recvfrom(BUFFER_SIZE) #listening till receiving the datagram
-            CMD = BYTE_ADDRESS_PAIRS[0].decode('utf-8')
+            CMD = BYTE_ADDRESS_PAIRS[0].decode().split('|')
+            REQ_TYPE = CMD[0]
+            REQ_SEQNO = CMD[1]
+            REQ_EXT = CMD[2]
+            REQ_SIZE = CMD[3]
             ADDR = BYTE_ADDRESS_PAIRS[1]
-            print(REQ_MSG.format(ADDR[0],ADDR[1],CMD))
+            print(REQ_MSG.format(ADDR[0],ADDR[1],BYTE_ADDRESS_PAIRS[0].decode()))
             try:
-                RES = str.encode(respond(CMD))
-                UDPServerSocket.sendto(RES,ADDR)
+                UDPServerSocket.sendto(genCMD(DATAGRAM_TYPE.ACK,int(REQ_SEQNO)+1,BUFFER_SIZE),ADDR)
+                
             except KeyboardInterrupt:
                 UDPServerSocket.sendto(str.encode(KI_CMD),ADDR)
                 print('! KEYBOARD INTERRUPT ')
@@ -117,27 +59,46 @@ if __name__ == '__main__':
                 print(ex.msg)
                 print(EXIT_MSG)
                 break
-            except EmptyInput as ex:
-                UDPServerSocket.sendto(str.encode(ex.msg),ADDR)
-                print(ex.msg)
-            except InvalidInput as ex:
-                UDPServerSocket.sendto(str.encode(ex.msg),ADDR)
-                print(ex.msg)
-            except InvalidOperator as ex:
-                UDPServerSocket.sendto(str.encode(ex.msg+ex.opt),ADDR)
-                print(ex.msg+ex.opt)
-            except InvalidArgs as ex:
-                UDPServerSocket.sendto(str.encode(ex.msg+ex.arg),ADDR)
-                print(ex.msg+ex.arg)
-            except ZeroDivisionError as ex:
-                UDPServerSocket.sendto(str.encode('\t ! ' + str(ex).upper()),ADDR)
-                print(ex)
-            except ArithmeticError as ex:
-                UDPServerSocket.sendto(str.encode('\t ! ' + str(ex).upper()),ADDR)
-                print(ex)
-            except Exception as ex:
-                UDPServerSocket.sendto(str.encode('\t SERVER ERROR ' + str(ex).upper()),ADDR)
-                print(ex)
+            res=str.encode('')
+            while len(res)<int(REQ_SIZE):
+                BYTE_ADDRESS_PAIRS = UDPServerSocket.recvfrom(BUFFER_SIZE) #listening till receiving the datagram
+                socket.setdefaulttimeout(2.5) # checks if the client gave up on sending
+                CMD = BYTE_ADDRESS_PAIRS[0].decode().split('|')
+                REQ_TYPE = CMD[0]
+                REQ_SEQNO_new = CMD[1]
+                if(int(REQ_SEQNO_new)<=int(REQ_SEQNO)):#discarding replicated packets
+                    continue
+                else:
+                    REQ_SEQNO = REQ_SEQNO_new
+                DATA = CMD[2]
+                res+=str.encode(DATA)
+                ADDR = BYTE_ADDRESS_PAIRS[1]
+                print('\t',BYTE_ADDRESS_PAIRS[0].decode())
+                try:
+                    UDPServerSocket.sendto(genCMD(DATAGRAM_TYPE.ACK,int(REQ_SEQNO)+1),ADDR)
+                except socket.timeout: # listens for new client
+                    break
+                except KeyboardInterrupt:
+                    UDPServerSocket.sendto(str.encode(KI_CMD),ADDR)
+                    print('! KEYBOARD INTERRUPT ')
+                    print(EXIT_MSG)
+                    break
+                except QuitCommandEx as ex:
+                    UDPServerSocket.sendto(str.encode(ex.msg),ADDR)
+                    print(ex.msg)
+                    print(EXIT_MSG)
+                    break
+            print(res.decode())
+            t = PATH+str(random.randint(0,10000000000))+'.'+REQ_EXT
+            open(t, 'a').close()
+            f = open(t, 'wb')
+            f.write(res)
+            f.close()
+            
+
+            # except Exception as ex:
+            #     UDPServerSocket.sendto(str.encode('\t SERVER ERROR ' + str(ex).upper()),ADDR)
+            #     print(ex)
         UDPServerSocket.close()
     except socket.timeout as ex:
         print(TIMEOUT_MSG.format(TIMEOUT))
